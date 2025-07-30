4de40270e9dfc7819eff065ded52a44c70cb4f9b
Matheus <matheusfffcorrea@gmail.com>
2025-07-30 08:00:44 -0300

    fix: tamanho do array corrigido de 5 para 100

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PDV_Bug_Example
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lstProdutosDisponiveis = new System.Windows.Forms.ListBox();
+            this.lstCarrinho = new System.Windows.Forms.ListBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnFinalizarVenda = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotalItens = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstProdutosDisponiveis
+            // 
+            this.lstProdutosDisponiveis.FormattingEnabled = true;
+            this.lstProdutosDisponiveis.ItemHeight = 20;
+            this.lstProdutosDisponiveis.Location = new System.Drawing.Point(25, 55);
+            this.lstProdutosDisponiveis.Name = "lstProdutosDisponiveis";
+            this.lstProdutosDisponiveis.Size = new System.Drawing.Size(200, 284);
+            this.lstProdutosDisponiveis.TabIndex = 0;
+            // 
+            // lstCarrinho
+            // 
+            this.lstCarrinho.FormattingEnabled = true;
+            this.lstCarrinho.ItemHeight = 20;
+            this.lstCarrinho.Location = new System.Drawing.Point(340, 55);
+            this.lstCarrinho.Name = "lstCarrinho";
+            this.lstCarrinho.Size = new System.Drawing.Size(200, 284);
+            this.lstCarrinho.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(235, 160);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(95, 40);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = ">>";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnFinalizarVenda
+            // 
+            this.btnFinalizarVenda.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnFinalizarVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizarVenda.ForeColor = System.Drawing.Color.White;
+            this.btnFinalizarVenda.Location = new System.Drawing.Point(340, 375);
+            this.btnFinalizarVenda.Name = "btnFinalizarVenda";
+            this.btnFinalizarVenda.Size = new System.Drawing.Size(200, 50);
+            this.btnFinalizarVenda.TabIndex = 3;
+            this.btnFinalizarVenda.Text = "Finalizar Venda";
+            this.btnFinalizarVenda.UseVisualStyleBackColor = false;
+            this.btnFinalizarVenda.Click += new System.EventHandler(this.btnFinalizarVenda_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Produtos Disponíveis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(340, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Carrinho de Compra";
+            // 
+            // lblTotalItens
+            // 
+            this.lblTotalItens.AutoSize = true;
+            this.lblTotalItens.Location = new System.Drawing.Point(340, 345);
+            this.lblTotalItens.Name = "lblTotalItens";
+            this.lblTotalItens.Size = new System.Drawing.Size(120, 20);
+            this.lblTotalItens.TabIndex = 6;
+            this.lblTotalItens.Text = "Total de Itens: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 444);
+            this.Controls.Add(this.lblTotalItens);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFinalizarVenda);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.lstCarrinho);
+            this.Controls.Add(this.lstProdutosDisponiveis);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDV Exemplo - Aula 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ListBox lstProdutosDisponiveis;
+        private System.Windows.Forms.ListBox lstCarrinho;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnFinalizarVenda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotalItens;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PDV_Bug_Example
+{
+    public partial class Form1 : Form
+    {
+        private List<string> itensVenda = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Inicializa a lista de produtos disponíveis
+            lstProdutosDisponiveis.Items.Add("Produto A");
+            lstProdutosDisponiveis.Items.Add("Produto B");
+            lstProdutosDisponiveis.Items.Add("Produto C");
+            lstProdutosDisponiveis.Items.Add("Produto D");
+            lstProdutosDisponiveis.Items.Add("Produto E");
+            lstProdutosDisponiveis.Items.Add("Produto F");
+            lstProdutosDisponiveis.Items.Add("Produto G");
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (lstProdutosDisponiveis.SelectedItem != null)
+            {
+                string produtoSelecionado = lstProdutosDisponiveis.SelectedItem.ToString();
+                itensVenda.Add(produtoSelecionado);
+                AtualizarCarrinho();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecione um produto para adicionar.", "Aviso");
+            }
+        }
+
+        private void AtualizarCarrinho()
+        {
+            lstCarrinho.Items.Clear();
+            foreach (var item in itensVenda)
+            {
+                lstCarrinho.Items.Add(item);
+            }
+            lblTotalItens.Text = $"Total de Itens: {itensVenda.Count}";
+        }
+
+
+        private void btnFinalizarVenda_Click(object sender, EventArgs e)
+        {
+            if (itensVenda.Count == 0)
+            {
+                MessageBox.Show("O carrinho está vazio!", "Erro");
+                return;
+            }
+
+            // O erro está aqui: o array foi inicializado com um tamanho fixo de 5.
+            // Se 'itensVenda' tiver 6 ou mais itens, ocorrerá um IndexOutOfRangeException.
+            string[] itensParaProcessar = new string[5];
+
+            try
+            {
+                for (int i = 0; i < itensVenda.Count; i++)
+                {
+                    // A exceção ocorrerá aqui quando 'i' for 5.
+                    itensParaProcessar[i] = itensVenda[i].ToUpper();
+                    Console.WriteLine($"Processando: {itensParaProcessar[i]}");
+                }
+
+                MessageBox.Show("Venda finalizada com sucesso!", "Sucesso");
+                itensVenda.Clear();
+                AtualizarCarrinho();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                // Em um cenário real, o debugger do Visual Studio pausaria na linha do erro
+                // antes mesmo de chegar no catch, se não estiver dentro de um try-catch.
+                MessageBox.Show(
+                    "Ocorreu um erro crítico ao processar a venda!\n\n" +
+                    "Detalhes para o desenvolvedor:\n" +
+                    $"Erro: {ex.Message}\n" +
+                    "O sistema tentou acessar uma posição de memória inválida. Verifique o tamanho do array 'itensParaProcessar'.",
+                    "ERRO FATAL",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+    }
+}
+        
